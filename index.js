06217cbc406ffe2aebf5f14ed50f0cb8391a6516
Isaac Daniel Batista <dbautista@denumeris.com>
2017-01-02 11:10:27 -0600

    Inicio de API REST con nodeJS, Express y MongoDB

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use( bodyParser.urlencoded( { extended: false } ) )
+app.use( bodyParser.json() )
+
+app.get( '/api/product', ( req, res ) => {
+	res.send( 200, { products: [] } )
+} )
+
+app.get( '/api/product/:productId', ( req, res ) => {
+
+} )
+
+app.post( '/api/product', ( req, res ) => {
+	console.log( req.body )
+	res.status( 200 ).send( { message: 'El producto se ha recibido' } )
+} )
+
+app.put( '/api/product/:productId', ( req, res ) => {
+	
+} )
+
+app.delete( '/api/product/:productId', ( req, res ) => {
+
+} )
+
+mongoose.connect('mongodb://localhost:27017/shop', ( err, res ) => {
+	if( err )
+	{
+		return console.log( `Error al conectar a la basxe de datos: ${err}` )
+	}
+	console.log( 'Conexión a la base de datos establecida. ' ) 
+	app.listen( 3000, () => {
+		console.log(`API REST corriendo en http://localhost:${port}`)
+	})
+})
